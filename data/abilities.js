8d5845036ed5ebfbe5f818dd2ad856ff856ba8da
Jeffrey Veenhuis <jeffroiscool@gmail.com>
2020-02-03 15:41:33 +0100

    Changes to colors

--- data/abilities.js
+++ data/abilities.js
@@ -5,32 +5,42 @@ var abilityList = [
         id: 838,
         name: "Combust",
         type: "DoT",
-        icon: "/i/013000/013213.png"
+        icon: "/i/013000/013213.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     {
         id: 843,
         name: "Combust II",
         type: "DoT",
-        icon: "/i/013000/013214.png"
+        icon: "/i/013000/013214.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     {
         id: 1881,
         name: "Combust III",
         type: "DoT",
-        icon: "/i/013000/013248.png"
+        icon: "/i/013000/013248.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     // Self-Buffs
     {
         id: 1878,
         name: "Divination",
         type: "Buff",
-        icon: "/i/013000/013245.png"
+        icon: "/i/013000/013245.png",
+        color: "--filter-light-gold",
+        order: 0
     },
     {
         id: 841,
         name: "Lightspeed",
         type: "Buff",
-        icon: "/i/013000/013220.png"
+        icon: "/i/013000/013220.png",
+        color: "--filter-light-gold",
+        order: 1
     },
     // BLM
     // DoTs
@@ -38,25 +48,33 @@ var abilityList = [
         id: 161,
         name: "Thunder",
         type: "DoT",
-        icon: "/i/010000/010457.png"
+        icon: "/i/010000/010457.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     {
         id: 162,
         name: "Thunder II",
         type: "DoT",
-        icon: "/i/010000/010458.png"
+        icon: "/i/010000/010458.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     {
         id: 163,
         name: "Thunder III",
         type: "DoT",
-        icon: "/i/010000/010459.png"
+        icon: "/i/010000/010459.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     {
         id: 1210,
         name: "Thunder IV",
         type: "DoT",
-        icon: "/i/012000/012657.png"
+        icon: "/i/012000/012657.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     // BRD
     // DoTs
@@ -64,32 +82,42 @@ var abilityList = [
         id: 124,
         name: "Venomous Bite",
         type: "DoT",
-        icon: "/i/010000/010352.png"
+        icon: "/i/010000/010352.png",
+        color: "--filter-light-pink",
+        order: 0
     },
     {
         id: 129,
         name: "Windbite",
         type: "DoT",
-        icon: "/i/010000/010360.png"
+        icon: "/i/010000/010360.png",
+        color: "--filter-baby-blue",
+        order: 1
     },
     {
         id: 1201,
         name: "Caustic Bite",
         type: "DoT",
-        icon: "/i/012000/012617.png"
+        icon: "/i/012000/012617.png",
+        color: "--filter-light-pink",
+        order: 0
     },
     {
         id: 1200,
         name: "Stormbite",
         type: "DoT",
-        icon: "/i/012000/012616.png"
+        icon: "/i/012000/012616.png",
+        color: "--filter-baby-blue",
+        order: 1
     },
     // Self-Buffs
     {
         id: 125,
         name: "Raging Strikes",
         type: "Buff",
-        icon: "/i/010000/010354.png"
+        icon: "/i/010000/010354.png",
+        color: "--filter-dark-red",
+        order: 0
     },
     // DNC
     // Self-Buffs
@@ -97,51 +125,68 @@ var abilityList = [
         id: 1825,
         name: "Devilment",
         type: "Buff",
-        icon: "/i/013000/013714.png"
+        icon: "/i/013000/013714.png",
+        color: "--filter-dark-green",
+        order: 4
     },
     {
         id: 1827,
         name: "Improvisation",
         type: "Buff",
-        icon: "/i/013000/013716.png"
+        icon: "/i/013000/013716.png",
+        color: "--filter-jungle-green",
+        order: 3
     },
     {
         id: 1821,
         name: "Standard Finish",
         type: "Buff",
-        icon: "/i/013000/013708.png"
+        icon: "/i/013000/013708.png",
+        color: "--filter-green-blue",
+        order: 1
     },
     {
-        id: 1827,
-        name: "Devilment",
+        id: 1822,
+        name: "Technical Finish",
         type: "Buff",
-        icon: "/i/013000/013716.png"
+        icon: "/i/013000/013709.png",
+        color: "--filter-bright-red",
+        order: 2
     },
     // DRG
     // DoTs
     {
         id: 118,
         name: "Chaos Thrust",
-        type: "DoT"
+        type: "DoT",
+        icon: "/i/010000/010307.png",
+        color: "--filter-light-purple",
+        order: 0
     },
     // Self-Buffs
     {
         id: 121,
         name: "Disembowel",
         type: "Buff",
-        icon: "/i/010000/010307.png"
+        icon: "/i/012000/012576.png",
+        color: "--filter-grey",
+        order: 0
     },
     {
         id: 1183,
         name: "Dragon Sight",
         type: "Buff",
-        icon: "/i/012000/012581.png"
+        icon: "/i/012000/012581.png",
+        color: "--filter-dark-red",
+        order: 1
     },
     {
         id: 1864,
         name: "Lance Charge",
         type: "Buff",
-        icon: "/i/010000/010304.png"
+        icon: "/i/010000/010304.png",
+        color: "--filter-dark-red",
+        order: 2
     },
     // DRK
     // Buffs
@@ -149,13 +194,17 @@ var abilityList = [
         id: 742,
         name: "Blood Weapon",
         type: "Buff",
-        icon: "/i/013000/013109.png"
+        icon: "/i/013000/013109.png",
+        color: "--filter-dark-red",
+        order: 0
     },
     {
         id: 1972,
         name: "Delirium",
         type: "Buff",
-        icon: "/i/013000/013121.png"
+        icon: "/i/013000/013121.png",
+        color: "--filter-bright-red",
+        order: 1
     },
     // GNB
     // Buffs
@@ -163,7 +212,9 @@ var abilityList = [
         id: 1831,
         name: "No Mercy",
         type: "Buff",
-        icon: "/i/013000/013601.png"
+        icon: "/i/013000/013601.png",
+        color: "--filter-light-blue",
+        order: 0
     },
     // MCH
     // DoTs
@@ -171,20 +222,26 @@ var abilityList = [
         id: 1866,
         name: "Bioblaster",
         type: "DoT",
-        icon: "/i/013000/013020.png"
+        icon: "/i/013000/013020.png",
+        color: "--filter-toxic-green",
+        order: 0
     },
     // Buffs
     {
         id: 688,
         name: "Hypercharge",
         type: "Buff",
-        icon: "/i/013000/013012.png"
+        icon: "/i/013000/013012.png",
+        color: "--filter-dark-green",
+        order: 0
     },
     {
         id: 1946,
         name: "Wildfire",
         type: "Buff",
-        icon: "/i/013000/013019.png"
+        icon: "/i/013000/013019.png",
+        color: "--filter-orange",
+        order: 1
     },
     // MNK
     // DoTs
@@ -192,20 +249,26 @@ var abilityList = [
         id: 246,
         name: "Demolish",
         type: "DoT",
-        icon: "/i/010000/010218.png"
+        icon: "/i/010000/010218.png",
+        color: "--filter-light-purple",
+        order: 0
     },
     // Buffs
     {
         id: 592,
         name: "Ring of Fire",
         type: "Buff",
-        icon: "/i/015000/015628.png"
+        icon: "/i/015000/015628.png",
+        color: "--filter-bright-red",
+        order: 0
     },
     {
         id: 101,
         name: "Twin Snakes",
         type: "Buff",
-        icon: "/i/010000/010216.png"
+        icon: "/i/010000/010216.png",
+        color: "--filter-grey",
+        order: 1
     },
     // NIN
     // DoTs
@@ -213,14 +276,18 @@ var abilityList = [
         id: 508,
         name: "Shadow Fang",
         type: "DoT",
-        icon: "/i/010000/010612.png"
+        icon: "/i/010000/010612.png",
+        color: "--filter-baby-blue",
+        order: 0
     },
     // Buffs
     {
         id: 500,
         name: "Huton",
         type: "Buff",
-        icon: "/i/012000/012903.png"
+        icon: "/i/012000/012903.png",
+        color: "--filter-light-blue",
+        order: 1
     },
     // PLD
     // DoTs
@@ -228,26 +295,34 @@ var abilityList = [
         id: 248,
         name: "Circle of Scorn",
         type: "DoT",
-        icon: "/i/010000/010158.png"
+        icon: "/i/010000/010158.png",
+        color: "--filter-orange",
+        order: 0
     },
     {
         id: 725,
         name: "Goring Blade",
         type: "DoT",
-        icon: "/i/012000/012507.png"
+        icon: "/i/012000/012507.png",
+        color: "--filter-orange",
+        order: 1
     },
     // Buffs
     {
         id: 76,
         name: "Fight or Flight",
         type: "Buff",
-        icon: "/i/010000/010155.png"
+        icon: "/i/010000/010155.png",
+        color: "--filter-light-purple",
+        order: 0
     },
     {
         id: 1368,
         name: "Requiescat",
         type: "Buff",
-        icon: "/i/012000/012514.png"
+        icon: "/i/012000/012514.png",
+        color: "--filter-dark-blue",
+        order: 1
     },
     // SAM
     // DoTs
@@ -255,20 +330,26 @@ var abilityList = [
         id: 1228,
         name: "Higanbana",
         type: "DoT",
-        icon: "/i/013000/013304.png"
+        icon: "/i/013000/013304.png",
+        color: "--filter-orange",
+        order: 0
     },
     // Buffs
     {
         id: 1298,
         name: "Jinpu",
         type: "Buff",
-        icon: "/i/013000/013301.png"
+        icon: "/i/013000/013301.png",
+        color: "--filter-light-gold",
+        order: 0
     },
     {
         id: 1299,
         name: "Shifu",
         type: "Buff",
-        icon: "/i/013000/013302.png"
+        icon: "/i/013000/013302.png",
+        color: "--filter-grey",
+        order: 1
     },
     // SCH
     // DoTs
@@ -276,19 +357,25 @@ var abilityList = [
         id: 179,
         name: "Bio",
         type: "DoT",
-        icon: "/i/010000/010504.png"
+        icon: "/i/010000/010504.png",
+        color: "--filter-dark-green",
+        order: 0
     },
     {
         id: 189,
         name: "Bio II",
         type: "DoT",
-        icon: "/i/010000/010505.png"
+        icon: "/i/010000/010505.png",
+        color: "--filter-dark-green",
+        order: 0
     },
     {
         id: 1895,
         name: "Biolysis",
         type: "DoT",
-        icon: "/i/012000/012812.png"
+        icon: "/i/012000/012812.png",
+        color: "--filter-baby-blue",
+        order: 0
     },
     // SMN
     // DoTs
@@ -296,37 +383,49 @@ var abilityList = [
         id: 179,
         name: "Bio",
         type: "DoT",
-        icon: "/i/010000/010504.png"
+        icon: "/i/010000/010504.png",
+        color: "--filter-dark-green",
+        order: 0
     },
     {
         id: 189,
         name: "Bio II",
         type: "DoT",
-        icon: "/i/010000/010505.png"
+        icon: "/i/010000/010505.png",
+        color: "--filter-dark-green",
+        order: 0
     },
     {
         id: 1214,
         name: "Bio III",
         type: "DoT",
-        icon: "/i/012000/012682.png"
+        icon: "/i/012000/012682.png",
+        color: "--filter-dark-green",
+        order: 0
     },
     {
         id: 180,
         name: "Miasma",
         type: "DoT",
-        icon: "/i/010000/010506.png"
+        icon: "/i/010000/010506.png",
+        color: "--filter-baby-blue",
+        order: 1
     },
     {
         id: 188,
         name: "Miasma II",
         type: "DoT",
-        icon: "/i/010000/010507.png"
+        icon: "/i/010000/010507.png",
+        color: "--filter-baby-blue",
+        order: 1
     },
     {
         id: 1215,
         name: "Miasma III",
         type: "DoT",
-        icon: "/i/012000/012683.png"
+        icon: "/i/012000/012683.png",
+        color: "--filter-baby-blue",
+        order: 1
     },
     // WAR
     // Buffs
@@ -334,19 +433,25 @@ var abilityList = [
         id: 86,
         name: "Berserk",
         type: "Buff",
-        icon: "/i/010000/010253.png"
+        icon: "/i/010000/010253.png",
+        color: "--filter-bright-red",
+        order: 0
     },
     {
         id: 1177,
         name: "Inner Release",
         type: "Buff",
-        icon: "/i/012000/012556.png"
+        icon: "/i/012000/012556.png",
+        color: "--filter-bright-red",
+        order: 1
     },
     {
         id: 90,
         name: "Storm's Eye",
         type: "Buff",
-        icon: "/i/010000/010263.png"
+        icon: "/i/010000/010263.png",
+        color: "--filter-light-purple",
+        order: 2
     },
     // WHM
     // DoTs
@@ -354,31 +459,41 @@ var abilityList = [
         id: 143,
         name: "Aero",
         type: "DoT",
-        icon: "/i/010000/010403.png"
+        icon: "/i/010000/010403.png",
+        color: "--filter-green-blue",
+        order: 0
     },
     {
         id: 144,
         name: "Aero II",
         type: "DoT",
-        icon: "/i/010000/010409.png"
+        icon: "/i/010000/010409.png",
+        color: "--filter-green-blue",
+        order: 0
     },
     {
         id: 798,
         name: "Aero III",
         type: "DoT",
-        icon: "/i/012000/012630.png"
+        icon: "/i/012000/012630.png",
+        color: "--filter-green-blue",
+        order: 0
     },
     {
         id: 1871,
         name: "Dia",
         type: "DoT",
-        icon: "/i/012000/012635.png"
+        icon: "/i/012000/012635.png",
+        color: "--filter-baby-blue",
+        order: 0
     },
     // Buffs
     {
         id: 157,
         name: "Presence of Mind",
         type: "Buff",
-        icon: "/i/012000/012627.png"
+        icon: "/i/012000/012627.png",
+        color: "--filter-baby-blue",
+        order: 0
     },
 ]
