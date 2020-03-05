6119427efcaabfd6215d14548194ecf28629c474
Jeffrey Veenhuis <jeffroiscool@gmail.com>
2020-03-05 12:23:02 +0100

    Changes to layout of the language file

--- data/language/en.js
+++ data/language/en.js
@@ -1,278 +1,77 @@
 var language = [
-	{
-		id: "settingsheader",
-		string: "Settings"
-	},
-	{
-		id: "generaloptions",
-		string: "General Options"
-	},
-	{
-		id: "generaldescription",
-		string: "Here you can find general options for ZeffUI."
-	},
-	{
-		id: "skin",
-		string: "Skin"
-	},
-	{
-		id: "healthbar",
-		string: "Healthbar"
-	},
-	{
-		id: "healthbardescription",
-		string: "Here you can set your options for your healthbar."
-	},
-	{
-		id: "posx",
-		string: "Position X"
-	},
-	{
-		id: "posy",
-		string: "Position Y"
-	},
-	{
-		id: "scale",
-		string: "Scale"
-	},
-	{
-		id: "hideoutofcombat",
-		string: "Hide out of Combat"
-	},
-	{
-		id: "healthtext",
-		string: "Health Text"
-	},
-	{
-		id: "textformat",
-		string: "Text Format"
-	},
-	{
-		id: "color",
-		string: "Color"
-	},
-	{
-		id: "example",
-		string: "Example"
-	},
-	{
-		id: "manabar",
-		string: "Manabar"
-	},
-	{
-		id: "manabardescription",
-		string: "Here you can set your options for your manabar."
-	},
-	{
-		id: "manatext",
-		string: "Mana Text"
-	},
-	{
-		id: "bufftimers",
-		string: "Buff Timers"
-	},
-	{
-		id: "bufftimersdescription",
-		string: "Here you can set your options for your personal buffs."
-	},
-	{
-		id: "growdirection",
-		string: "Grow Direction"
-	},
-	{
-		id: "down",
-		string: "Down"
-	},
-	{
-		id: "up",
-		string: "Up"
-	},
-	{
-		id: "left",
-		string: "Left"
-	},
-	{
-		id: "right",
-		string: "Right"
-	},
-	{
-		id: "padding",
-		string: "Padding"
-	},
-	{
-		id: "image",
-		string: "Image"
-	},
-	{
-		id: "droppedoff",
-		string: "Hide when dropped off"
-	},
-	{
-		id: "bufftext",
-		string: "Buff Text"
-	},
-	{
-		id: "dottimers",
-		string: "DoT Timers"
-	},
-	{
-		id: "dottimersdescription",
-		string: "Here you can set your options for your personal debuffs."
-	},
-	{
-		id: "multidot",
-		string: "MultiDoT (not implemented yet)"
-	},
-	{
-		id: "dottext",
-		string: "DoT Text"
-	},
-	{
-		id: "pulltimers",
-		string: "Pulltimers"
-	},
-	{
-		id: "pulltimersdescription",
-		string: "Here you can set your options for the pulltimer."
-	},
-	{
-		id: "pulltimertext",
-		string: "Pulltimer Text"
-	},
-	{
-		id: "stacksbar",
-		string: "Stacks Bar"
-	},
-	{
-		id: "stacksbardescription",
-		string: "Here you can set your options for your personal stacks"
-	},
-	{
-		id: "raidbuffs",
-		string: "Raid Buffs"
-	},
-	{
-		id: "raidbuffdescription",
-		string: "Here you can set your options for the raidbuffs (DPS Buffs/Debuffs only)."
-	},
-	{
-		id: "columns",
-		string: "Columns per row"
-	},
-	{
-		id: "growleft",
-		string: "Grow icons to the left"
-	},
-	{
-		id: "hidesolo",
-		string: "Hide when not in Party"
-	},
-	{
-		id: "alwaysshow",
-		string: "Always show abilities (when not on cooldown)"
-	},
-	{
-		id: "mitigations",
-		string: "Mitigations"
-	},
-	{
-		id: "mitigationdescription",
-		string: "Here you can set your options for your mitigation bar."
-	},
-	{
-		id: "savesettings",
-		string: "Save Settings"
-	},
-	{
-		id: "skindefault",
-		string: "Default (by Square Enix)"
-	},
-	{
-		id: "skinmaterialdark",
-		string: "Material Dark (by skotlex)"
-	},
-	{
-		id: "skinmaterialdiscord",
-		string: "Material Discord (by skotlex)"
-	},
-	{
-		id: "colordarkred",
-		string: "Dark Red"
-	},
-	{
-		id: "colorbrightred",
-		string: "Bright Red"
-	},
-	{
-		id: "colorbabyblue",
-		string: "Baby Blue"
-	},
-	{
-		id: "colorgreenblue",
-		string: "Green Blue"
-	},
-	{
-		id: "colortoxicgreen",
-		string: "Toxic Green"
-	},
-	{
-		id: "colorlightblue",
-		string: "Light Blue"
-	},
-	{
-		id: "colordarkblue",
-		string: "Dark Blue"
-	},
-	{
-		id: "colordarkgreen",
-		string: "Dark Green"
-	},
-	{
-		id: "colorjunglegreen",
-		string: "Jungle Green"
-	},
-	{
-		id: "colorpurple",
-		string: "Purple"
-	},
-	{
-		id: "colorfuchsia",
-		string: "Fuchsia"
-	},
-	{
-		id: "colorlightpink",
-		string: "Light Pink"
-	},
-	{
-		id: "colorlightgold",
-		string: "Light Gold"
-	},
-	{
-		id: "colordarkgold",
-		string: "Dark Gold"
-	},
-	{
-		id: "colororange",
-		string: "Orange"
-	},
-	{
-		id: "colorgrey",
-		string: "Grey"
-	},
-	{
-		id: "lock",
-		string: "Lock/Unlock Bars"
-	},
-	{
-		id: "grid",
-		string: "Toggle Grid"
-	},
-	{
-		id: "settings",
-		string: "Settings"
-	},
-	{
-		id: "close",
-		string: "Close"
-	}
+	{ id: "settingsheader", string: "Settings" },
+	{ id: "savesettings", string: "Save Settings" },
+	{ id: "generaloptions", string: "General Options" },
+	// COMPONENTS
+	{ id: "healthbar", string: "Healthbar" },
+	{ id: "manabar", string: "Manabar" },
+	{ id: "pulltimers", string: "Pulltimers" },
+	{ id: "bufftimers", string: "Buff Timers" },
+	{ id: "dottimers", string: "DoT Timers" },
+	{ id: "stacksbar", string: "Stacks Bar" },
+	{ id: "raidbuffs", string: "Raid Buffs" },
+	{ id: "skin", string: "Skin" },
+	{ id: "mitigations", string: "Mitigations" },
+	// DESCRIPTIONS
+	{ id: "generaldescription", string: "Here you can find general options for ZeffUI." },
+	{ id: "healthbardescription", string: "Here you can set your options for your healthbar." },
+	{ id: "manabardescription", string: "Here you can set your options for your manabar." },
+	{ id: "bufftimersdescription", string: "Here you can set your options for your personal buffs." },
+	{ id: "dottimersdescription", string: "Here you can set your options for your personal debuffs." },
+	{ id: "pulltimersdescription", string: "Here you can set your options for the pulltimer." },
+	{ id: "stacksbardescription", string: "Here you can set your options for your personal stacks" },
+	{ id: "raidbuffdescription", string: "Here you can set your options for the raidbuffs (DPS Buffs/Debuffs only)." },
+	{ id: "mitigationdescription", string: "Here you can set your options for your mitigation bar." },
+	// COMPONENT PROPERTIES
+	{ id: "posx", string: "Position X" },
+	{ id: "posy", string: "Position Y" },
+	{ id: "scale", string: "Scale" },
+	{ id: "padding", string: "Padding" },
+	{ id: "color", string: "Color" },
+	{ id: "image", string: "Image" },
+	{ id: "example", string: "Example" },
+	{ id: "growleft", string: "Grow icons to the left" },
+	{ id: "growdirection", string: "Grow Direction" },
+	{ id: "down", string: "Down" },
+	{ id: "up", string: "Up" },
+	{ id: "left", string: "Left" },
+	{ id: "right", string: "Right" },
+	{ id: "columns", string: "Columns per row" },
+	{ id: "hideoutofcombat", string: "Hide out of Combat" },
+	{ id: "hidesolo", string: "Hide when not in Party" },
+	{ id: "droppedoff", string: "Hide when dropped off" },
+	{ id: "alwaysshow", string: "Always show abilities (when not on cooldown)" },
+	{ id: "textformat", string: "Text Format" },
+	{ id: "healthtext", string: "Health Text" },
+	{ id: "manatext", string: "Mana Text" },
+	{ id: "pulltimertext", string: "Pulltimer Text" },
+	{ id: "bufftext", string: "Buff Text" },
+	{ id: "dottext", string: "DoT Text" },
+	{ id: "multidot", string: "MultiDoT (not implemented yet)" },
+	// SKINS
+	{ id: "skindefault", string: "Default (by Square Enix)" },
+	{ id: "skinmaterialdark", string: "Material Dark (by skotlex)" },
+	{ id: "skinmaterialdiscord", string: "Material Discord (by skotlex)" },
+	// COLORS
+	{ id: "colordarkred", string: "Dark Red" },
+	{ id: "colorbrightred", string: "Bright Red" },
+	{ id: "colorbabyblue", string: "Baby Blue" },
+	{ id: "colorgreenblue", string: "Green Blue" },
+	{ id: "colortoxicgreen", string: "Toxic Green" },
+	{ id: "colorlightblue", string: "Light Blue" },
+	{ id: "colordarkblue", string: "Dark Blue" },
+	{ id: "colordarkgreen", string: "Dark Green" },
+	{ id: "colorjunglegreen", string: "Jungle Green" },
+	{ id: "colorpurple", string: "Purple" },
+	{ id: "colorfuchsia", string: "Fuchsia" },
+	{ id: "colorlightpink", string: "Light Pink" },
+	{ id: "colorlightgold",	string: "Light Gold"  },
+	{ id: "colordarkgold", string: "Dark Gold" },
+	{ id: "colororange", string: "Orange" },
+	{ id: "colorgrey", string: "Grey" },
+	// CONTEXT MENU
+	{ id: "lock", string: "Lock/Unlock Bars" },
+	{ id: "grid", string: "Toggle Grid" },
+	{ id: "settings", string: "Settings" },
+	{ id: "close", string: "Close" }
 ];
