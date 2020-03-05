b7b4712bfe12442d95eae09d6a7dd16e5b58acb4
Jeffrey Veenhuis <jeffroiscool@gmail.com>
2020-03-05 21:31:56 +0100

    Change regex to .* until I can figure out better japanese regexes

--- data/regex.js
+++ data/regex.js
@@ -105,7 +105,7 @@ var regexList =
 		regex: "] 15:",
 		matches: [
 			{
-				"regex": "(?<playerid>[A-F0-9]{8}):(?<player>[a-zA-Z-' ]{2,31}):(?<skillid>[A-F0-9]{2,4}):(?<skillname>[-a-zA-Z' ]{2,31}):(?<targetid>[A-F0-9]{8})?:(?<target>[a-zA-Z-' ]{2,31})?:(?<power>\\d)?",
+				"regex": "(?<playerid>[A-F0-9]{8}):(?<player>[a-zA-Z-' ]{2,31}):(?<skillid>[A-F0-9]{2,4}):(?<skillname>.*):(?<targetid>[A-F0-9]{8})?:(?<target>[a-zA-Z-' ]{2,31})?:(?<power>\\d)?",
 				"function": "handleSkill"
 			}
 		]
