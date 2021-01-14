3a5c57fc4f1b99167ff8cc4775b7e04eb83ccbb4
Jeffrey Veenhuis <jeffroiscool@gmail.com>
2021-01-14 13:28:20 +0100

    Add PartyOrder, Sort Order, Override Order, fix Party Override

--- data/jobs.js
+++ data/jobs.js
@@ -3,60 +3,70 @@ var jobList = [
 	{
 		id: 0,
 		name: "NONE",
+		fullname: "None",
 		type: "None",
 		position_type: "None"
 	},
 	{
 		id: 1,
 		name: "GLA",
+		fullname: "Gladiator",
 		type: "Tank",
 		position_type: "Melee"
 	},
 	{
 		id: 2,
 		name: "PGL",
+		fullname: "Pugilist",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 3,
 		name: "MRD",
+		fullname: "Marauder",
 		type: "Tank",
 		position_type: "Melee"
 	},
 	{
 		id: 4,
 		name: "LNC",
+		fullname: "Lancer",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 5,
 		name: "ARC",
+		fullname: "Archer",
 		type: "DPS",
 		position_type: "Ranged"
 	},
 	{
 		id: 6,
 		name: "CNJ",
+		fullname: "Conjurer",
 		type: "Healer",
 		position_type: "Caster"
 	},
 	{
 		id: 7,
 		name: "THM",
+		fullname: "Thaumaturge",
 		type: "DPS",
 		position_type: "Caster"
 	},
 	{
 		id: 8,
 		name: "CRP",
+		fullname: "Carpenter",
 		type: "Crafter",
 		position_type: "None"
 	},
 	{
 		id: 9,
 		name: "BSM",
+		fullname: "Blacksmith",
 		type: "Crafter",
 		position_type: "None"
 	},
@@ -64,173 +74,202 @@ var jobList = [
 		id: 10,
 		name: "ARM",
 		type: "Crafter",
+		fullname: "Armorer",
 		position_type: "None"
 	},
 	{
 		id: 11,
 		name: "GSM",
+		fullname: "Goldsmith",
 		type: "Crafter",
 		position_type: "None"
 	},
 	{
 		id: 12,
 		name: "LTW",
+		fullname: "Leatherworker",
 		type: "Crafter",
 		position_type: "None"
 	},
 	{
 		id: 13,
 		name: "WVR",
+		fullname: "Weaver",
 		type: "Crafter",
 		position_type: "None"
 	},
 	{
 		id: 14,
 		name: "ALC",
+		fullname: "Alchemist",
 		type: "Crafter",
 		position_type: "None"
 	},
 	{
 		id: 15,
 		name: "CUL",
+		fullname: "Culinarian",
 		type: "Crafter",
 		position_type: "None"
 	},
 	{
 		id: 16,
 		name: "MIN",
+		fullname: "Miner",
 		type: "Gatherer",
 		position_type: "None"
 	},
 	{
 		id: 17,
 		name: "BTN",
+		fullname: "Botanist",
 		type: "Gatherer",
 		position_type: "None"
 	},
 	{
 		id: 18,
 		name: "FSH",
+		fullname: "Fisher",
 		type: "Gatherer",
 		position_type: "None"
 	},
 	{
 		id: 19,
 		name: "PLD",
+		fullname: "Paladin",
 		type: "Tank",
 		position_type: "Melee"
 	},
 	{
 		id: 20,
 		name: "MNK",
+		fullname: "Monk",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 21,
 		name: "WAR",
+		fullname: "Warrior",
 		type: "Tank",
 		position_type: "Melee"
 	},
 	{
 		id: 22,
 		name: "DRG",
+		fullname: "Dragoon",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 23,
 		name: "BRD",
+		fullname: "Bard",
 		type: "DPS",
 		position_type: "Ranged"
 	},
 	{
 		id: 24,
 		name: "WHM",
+		fullname: "White Mage",
 		type: "Healer",
 		position_type: "Caster"
 	},
 	{
 		id: 25,
 		name: "BLM",
+		fullname: "Black Mage",
 		type: "DPS",
 		position_type: "Caster"
 	},
 	{
 		id: 26,
 		name: "ACN",
+		fullname: "Arcanist",
 		type: "DPS",
 		position_type: "Caster"
 	},
 	{
 		id: 27,
 		name: "SMN",
+		fullname: "Summoner",
 		type: "DPS",
 		position_type: "Caster"
 	},
 	{
 		id: 28,
 		name: "SCH",
+		fullname: "Scholar",
 		type: "Healer",
 		position_type: "Caster"
 	},
 	{
 		id: 29,
 		name: "ROG",
+		fullname: "Rogue",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 30,
 		name: "NIN",
+		fullname: "Ninja",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 31,
 		name: "MCH",
+		fullname: "Machinist",
 		type: "DPS",
 		position_type: "Ranged"
 	},
 	{
 		id: 32,
 		name: "DRK",
+		fullname: "Dark Knight",
 		type: "Tank",
 		position_type: "Melee"
 	},
 	{
 		id: 33,
 		name: "AST",
+		fullname: "Astrologian",
 		type: "Healer",
 		position_type: "Caster"
 	},
 	{
 		id: 34,
 		name: "SAM",
+		fullname: "Samurai",
 		type: "DPS",
 		position_type: "Melee"
 	},
 	{
 		id: 35,
 		name: "RDM",
+		fullname: "Red Mage",
 		type: "DPS",
 		position_type: "Caster"
 	},
 	{
 		id: 36,
 		name: "BLU",
+		fullname: "Blue Mage",
 		type: "DPS",
 		position_type: "Caster"
 	},
 	{
 		id: 37,
 		name: "GNB",
+		fullname: "Gunblade",
 		type: "Tank",
 		position_type: "Melee"
 	},
 	{
 		id: 38,
 		name: "DNC",
+		fullname: "Dancer",
 		type: "DPS",
 		position_type: "Ranged"
 	}
